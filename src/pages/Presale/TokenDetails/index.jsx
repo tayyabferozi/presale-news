74047a27fd31c8ffcd27092682a85bd23ff94b49
Tayyab Ferozi <ferozitayyab@gmail.com>
2022-05-07 21:52:36 +0500

    complete

--- src/pages/Presale/TokenDetails/index.jsx
+++ src/pages/Presale/TokenDetails/index.jsx
@@ -61,16 +61,47 @@ const TokenDetails = () => {
               </div>
             </div>
           </div>
-          <div className="col-lg-6">
-            <div className="text-glowy-pink text-gradient-pink">
+          <div className="col-lg-6 p-relative">
+            <h3 className="fw-400 contribution-text text-glowy-pink text-gradient-pink">
               Contributions will close on DATE
-            </div>
+            </h3>
 
             <div className="box box-2">
               <div className="d-flex justify-content-between">
                 <div className="text-white">27%</div>
-                <div className="d-flex align-items-center gap-2">BSC</div>
+                <div className="d-flex align-items-center gap-2">
+                  <div className="logo-container">
+                    <img src="/assets/vectors/logo-bsc-2.svg" alt="bsc" />
+                  </div>
+                  BSC
+                </div>
               </div>
+
+              <div className="progress-wrap mt-10">
+                <div className="progress" style={{ width: "50%" }}></div>
+              </div>
+
+              <div className="btns mt-30">
+                <button className="btn">BUSD</button>
+                <button className="btn">OPEN</button>
+              </div>
+
+              <div className="hr mt-30"></div>
+
+              <div className="ranger">
+                <div className="ranger-left">500</div>
+                <div className="ranger-right">
+                  <div className="d-flex justify-content-between">
+                    <div className="text-white">Min $100</div>
+                    <div className="text-white">Max $10,000</div>
+                  </div>
+                  <div className="progress-wrap mt-10">
+                    <div className="progress" style={{ width: "50%" }}></div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="btn btn-contribute">CONTRIBUTE</div>
             </div>
           </div>
           <div className="col-xl-6">
@@ -83,35 +114,35 @@ const TokenDetails = () => {
               <h3>Token Information</h3>
 
               <div className="row gy-3 mt-4">
-                <div className="col-lg-8">
+                <div className="col-md-8">
                   <strong>Ticker :</strong>
                 </div>
-                <div className="col-lg-4">GQ</div>
+                <div className="col-md-4">GQ</div>
 
-                <div className="col-lg-8">
+                <div className="col-md-8">
                   <strong>Blockchain Network :</strong>
                 </div>
-                <div className="col-lg-4">2,005,000</div>
+                <div className="col-md-4">2,005,000</div>
 
-                <div className="col-lg-8">
+                <div className="col-md-8">
                   <strong>Initial Token Circulation : </strong>
                 </div>
-                <div className="col-lg-4">BSC</div>
+                <div className="col-md-4">BSC</div>
 
-                <div className="col-lg-8">
+                <div className="col-md-8">
                   <strong>Initial Market Cap :</strong>
                 </div>
-                <div className="col-lg-4">9,000,000 $</div>
+                <div className="col-md-4">9,000,000 $</div>
 
-                <div className="col-lg-8">
+                <div className="col-md-8">
                   <strong>Total Supply :</strong>
                 </div>
-                <div className="col-lg-4">10,000,000,000</div>
+                <div className="col-md-4">10,000,000,000</div>
 
-                <div className="col-lg-8">
+                <div className="col-md-8">
                   <strong>Contract Address :</strong>
                 </div>
-                <div className="col-lg-4">0х…</div>
+                <div className="col-md-4">0х…</div>
               </div>
             </div>
           </div>
@@ -125,42 +156,42 @@ const TokenDetails = () => {
               <h3>Conditions</h3>
 
               <div className="row gy-3 mt-4">
-                <div className="col-lg-6">
+                <div className="col-md-6">
                   <strong>Token price :</strong>
                 </div>
-                <div className="col-lg-6">GQ</div>
+                <div className="col-md-6">GQ</div>
 
-                <div className="col-lg-6">
+                <div className="col-md-6">
                   <strong>$0.012</strong>
                 </div>
-                <div className="col-lg-6">12.00%</div>
+                <div className="col-md-6">12.00%</div>
 
-                <div className="col-lg-6">
+                <div className="col-md-6">
                   <strong>Vesting :</strong>
                 </div>
-                <div className="col-lg-6">
+                <div className="col-md-6">
                   0% THE | CLIFF: 2 Months | VESTING: 24 Months
                 </div>
 
-                <div className="col-lg-6">
+                <div className="col-md-6">
                   <strong>Min. Contribution :</strong>
                 </div>
-                <div className="col-lg-6">$100</div>
+                <div className="col-md-6">$100</div>
 
-                <div className="col-lg-6">
+                <div className="col-md-6">
                   <strong>Max. Contribution :</strong>
                 </div>
-                <div className="col-lg-6">$10,000</div>
+                <div className="col-md-6">$10,000</div>
 
-                <div className="col-lg-6">
+                <div className="col-md-6">
                   <strong>Start date :</strong>
                 </div>
-                <div className="col-lg-6">...</div>
+                <div className="col-md-6">...</div>
 
-                <div className="col-lg-6">
+                <div className="col-md-6">
                   <strong>End date :</strong>
                 </div>
-                <div className="col-lg-6">...</div>
+                <div className="col-md-6">...</div>
               </div>
             </div>
           </div>
@@ -171,7 +202,7 @@ const TokenDetails = () => {
 
           <div className="info-head">
             <div className="tabs">
-              <div className="tab">Overview</div>
+              <div className="tab active">Overview</div>
               <div className="tab">Team</div>
               <div className="tab">Partners</div>
               <div className="tab">Roadmap</div>
@@ -191,11 +222,11 @@ const TokenDetails = () => {
 
             <h3 className="mt-50">Gamium is building:</h3>
 
-            <div className="d-flex gap-1 mt-10">
-              <strong className="text-white flex-shrink-0 mt-1">
+            <div className="d-sm-flex gap-1 mt-10">
+              <strong className="text-white flex-shrink-0 mt-1 d-inline-block">
                 The Avatar :{" "}
               </strong>
-              <p>
+              <p className="d-inline-block">
                 A digital identity that can be used across all web 3 and
                 metaverse applications, allowing unique interoperability and
                 usabillity.
